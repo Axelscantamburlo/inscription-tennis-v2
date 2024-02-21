1310b138e6da43cf8fd61db32f4fa9af51c4df0f
Axelscantamburlo <axel.scantamburlo31770@gmail.com>
2024-02-21 15:15:37 +0100

    message

--- src/pages/inscription/RegisterPlayeur/PlayeurRegistedInfo.js
+++ src/pages/inscription/RegisterPlayeur/PlayeurRegistedInfo.js
@@ -1,42 +1,46 @@
-import React, {useState, useEffect, useContext, useMemo} from "react";
+import React, { useState, useEffect, useContext, useMemo } from "react";
 
 // CONTEXT
 import { AllDataSchedules } from "../../../context/AllDataSchedules";
 // FUNCTION
-import { getPlayeurInscription, findPriceToPay } from "../../../functions/getPlayeurInscription";
+import {
+  getPlayeurInscription,
+  findPriceToPay,
+} from "../../../functions/getPlayeurInscription";
 import { useSelector } from "react-redux";
-
 
 export default function PlayeurRegistedInfo({ playeurInfo }) {
   const { name, birthDay, level, isPayed } = playeurInfo;
 
-  const {loadedData} = useContext(AllDataSchedules)
+  const { loadedData } = useContext(AllDataSchedules);
   // Appel d'une fonction externe pour récupérer les horaires d'inscriptions du joueur et les affichées
   const [playeurInscription, setPlayeurInscription] = useState([]);
-  const [priceToPay, setPriceToPay] = useState('')
+  const [priceToPay, setPriceToPay] = useState("");
   useEffect(() => {
-    const playExternFunction = async () =>  {
-      const inscriptions = await getPlayeurInscription(loadedData, name)
-      const price = findPriceToPay(inscriptions, level)
-      setPlayeurInscription(inscriptions)
-      setPriceToPay(price)
-    }
-    playExternFunction()
+    const playExternFunction = async () => {
+      const inscriptions = await getPlayeurInscription(loadedData, name);
+      const price = findPriceToPay(inscriptions, level);
+      setPlayeurInscription(inscriptions);
+      setPriceToPay(price);
+    };
+    playExternFunction();
   }, []);
-
+  console.log(playeurInscription);
   return (
     <div className="playeur-card">
       <h2>{name.toUpperCase()}</h2>
+      {playeurInscription.length === 0 && <p>Pas inscrit</p>}
       {playeurInscription.map((inscription, index) => {
-        return(
-            <div key={index}>
-                <p>Inscription: {inscription.day} de {inscription.startHour} à {inscription.endHour}</p>
-            </div>
-        )
+        return (
+          <div key={index}>
+            <p>
+              Inscription: {inscription.day} de {inscription.startHour} à{" "}
+              {inscription.endHour}
+            </p>
+          </div>
+        );
       })}
       {isPayed ? <p>Payé</p> : <p>Prix à payer: {priceToPay}</p>}
     </div>
   );
 }
-
-

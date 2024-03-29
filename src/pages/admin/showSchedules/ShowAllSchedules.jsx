b5dc73d6f6329b2d7a221fa453f8760a2ee54571
Axelscantamburlo <axel.scantamburlo31770@gmail.com>
2024-03-29 17:49:18 +0100

    message

--- src/pages/admin/showSchedules/ShowAllSchedules.jsx
+++ src/pages/admin/showSchedules/ShowAllSchedules.jsx
@@ -9,7 +9,7 @@ import ScheduleItemAdmin from "./ScheduleItemAdmin/ScheduleItemAdmin";
 
 export default function ShowAllSchedules() {
   const { loadedData } = useContext(AllDataSchedules);
-  const [sortBy, setSortBy] = useState("day");
+  const [sortBy, setSortBy] = useState("level");
 
   const sortedSchedules = loadedData?.sort((a, b) => {
     if (sortBy === "day") {
@@ -20,23 +20,27 @@ export default function ShowAllSchedules() {
       // console.log(levelOrder.indexOf(a.level) - levelOrder.indexOf(b.level));
       return levelOrder.indexOf(a.level) - levelOrder.indexOf(b.level);
     }
+    else if (sortBy === "numberOfPlaces") {
+      return a.numberOfPlaces - b.numberOfPlaces;
+    }
   });
 
   return (
     <div className="show-all-schedules-container">
       <NavBar toggleClassName={1} />
+      <div className="inputs">
       <select style={{marginTop: '70px'}} value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
-        <option value="day">Trier par jour</option>
         <option value="level">Trier par niveau</option>
+        <option value="day">Trier par jour</option>
+        <option value="numberOfPlaces">Nombre de places</option>
       </select>
+      </div>
       <div className="schedules-container">
       {sortedSchedules?.map((schedule, index) => (
-          <div key={index}>
-            {/* {index === 0 || schedule.day !== sortedSchedulesByDay[index - 1].day ? (
-              <h1>{schedule.day}</h1>
-            ) : null} */}
+       
+            <>
             <ScheduleItemAdmin schedule={schedule} />
-          </div>
+            </>
         ))}
       </div>
     </div>

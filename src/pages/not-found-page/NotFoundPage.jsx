e391148064526495412ef826dd695ed4958d7913
Axelscantamburlo <axel.scantamburlo31770@gmail.com>
2024-04-17 14:43:22 +0200

    message

--- src/pages/not-found-page/NotFoundPage.jsx
+++ src/pages/not-found-page/NotFoundPage.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
 
 const NotFoundPage = () => {
+  const navigate = useNavigate()
+  const userKey = localStorage.getItem('user')
+
+
   return (
-    <div>
+    <div className='not-found-page-container'>
+      <div className="content">
       <h1>Erreur 404</h1>
       <p>Désolé, la page que vous recherchez est introuvable.</p>
+      <button onClick={() => userKey ? navigate('/inscrire-un-joueur') : navigate('/')} >C'est noté</button>
+      </div>
     </div>
   );
 };
+
 
 export default NotFoundPage;
